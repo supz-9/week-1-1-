fb7529e3fed29f05612480c8b73c5a9cc7d4fbaf
supz-9 <supriya.23bec7057@vitapstudent.ac.in>
2025-06-07 17:11:42 +0530

    Add files via upload

---
+++ Sortarray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
